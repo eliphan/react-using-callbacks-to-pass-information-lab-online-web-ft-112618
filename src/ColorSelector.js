e32ee68afb96120f61a009b4cd8307780cb49488
Elise Phan <aliceconnic@gmail.com>
2019-04-13 18:35:55 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -5,7 +5,7 @@ export default class ColorSelector extends Component {
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
       
-  setSelectedColor = (str) => {
+  this.setSelectedColor = (str) => {
     color: this.props.str
   }   
   
